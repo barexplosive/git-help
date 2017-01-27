b9e12b14258167301ba10b0a93db45eb83e20089
Samvel <samvelbarstarzz@mail.ru>
2017-01-27 16:53:11 +0400

    edit

--- site for karen/index.php
+++ site for karen/index.php
@@ -3,6 +3,14 @@
 <head>
 	<title>Diarko Интернет-Магазин пищевых продуктов</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="style.css" href="style">
 </head>
-<body></body>
+<body>
+	<div class="Topmenu">
+		<link rel="stylesheet" type="text/css" href="">
+		<a href="">Контакты</a>
+		<a href="">Покупка и оплата</a>
+		<a href="">О нас</a>
+	</div>
+</body>
 </html>
